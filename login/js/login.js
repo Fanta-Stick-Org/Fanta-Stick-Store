6f1f83d90604c2d233aacafe7474acc0d2c05f63
Kevin <baquerochavarro@gmail.com>
2021-09-23 21:57:05 -0500

    login v1.0

---
+++ login/js/login.js
@@ -0,0 +1,17 @@
+const loginForm = document.getElementById("login-form");
+const loginButton = document.getElementById("login-form-submit");
+/* const loginErrorMsg = document.getElementById("login-error-msg"); */
+
+loginButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    const username = loginForm.username.value;
+    const password = loginForm.password.value;
+    
+    if (username === "admin" && password ==="admin") {
+        window.location.href="/vistas/index.html";
+        /* alert("Haz iniciado sesion correctamente"); */
+        /* location.reload(); */
+    } else {
+        loginErrorMsg.style.opacity = 1;
+    }
+})
