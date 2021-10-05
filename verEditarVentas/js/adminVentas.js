9c1d3bbe0f3c94986d5efc8c4f9f49e935b3422b
Rhaegar09 <edwardreinahortua@gmail.com>
2021-10-05 16:41:51 -0500

    Agregando rama ver o editar ventas

---
+++ verEditarVentas/js/adminVentas.js
@@ -0,0 +1,11 @@
+const buscar = document.getElementById('buscar');
+
+buscar.addEventListener('click', () => {
+    console.log('buscar');
+});
+
+
+function alerta() {
+    //const actualizar = document.getElementById('actualizar');
+    window.alert('La venta ha sido actualizada correctamente.');
+}
